0452eaaa5bf5afb6259e452764cc13168a6777a6
Ycow <skypnal12@gmail.com>
2023-07-04 20:54:42 +0900

    feat: 회원가입 파일 정리

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import useInput from '@hooks/useInput';
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import React, { ChangeEvent, FormEvent, useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
 
