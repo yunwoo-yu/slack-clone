9098de3beaff919a2950825e9e9702167a372086
Ycow <skypnal12@gmail.com>
2023-07-18 22:26:46 +0900

    feat: DM 날짜별 섹션 분리

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -9,6 +9,7 @@ import ChatList from '@components/ChatList';
 import useInput from '@hooks/useInput';
 import axios from 'axios';
 import { IDM } from '@typings/db';
+import makeSection from '@utils/makeSection';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams();
@@ -40,13 +41,15 @@ const DirectMessage = () => {
     return null;
   }
 
+  const chatSections = makeSection(chatData ? [...chatData].reverse() : []);
+
   return (
     <Container>
       <Header>
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatData={chatData} />
+      <ChatList chatSections={chatSections} />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
---
+++ utils/makeSection.ts
@@ -0,0 +1,20 @@
+import { IDM } from '@typings/db';
+import dayjs from 'dayjs';
+
+const makeSection = (chatList: IDM[]) => {
+  const sections: { [key: string]: IDM[] } = {};
+
+  chatList.forEach((chat) => {
+    const monthDate = dayjs(chat.createdAt).format('YYYY-MM-DD');
+
+    if (Array.isArray(sections[monthDate])) {
+      sections[monthDate].push(chat);
+    } else {
+      sections[monthDate] = [chat];
+    }
+  });
+
+  return sections;
+};
+
+export default makeSection;
