82b6e31d6bd2db08c65e200bded20e919f06e2a6
Ycow <skypnal12@gmail.com>
2023-07-18 22:26:13 +0900

    feat: 멘션 링크 기능 추가

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import styled from '@emotion/styled';
 import gravatar from 'gravatar';
 import { IDM } from '@typings/db';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link, useParams } from 'react-router-dom';
 
 interface Props {
   data: IDM;
@@ -11,24 +13,50 @@ interface Props {
 const Chat = ({ data }: Props) => {
   const user = data.Sender;
   const date = data.createdAt.toString();
+  const { workspace } = useParams();
+
+  // \d 숫자 + 는 1개 이상 ?는 0 또는 1개 *은 0개 이상
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+        decorator(match, index) {
+          const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+
+          console.log(arr);
+
+          if (arr) {
+            return (
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+
+          return <br key={index} />;
+        },
+      }),
+    [data.content],
+  );
 
   return (
     <ChatWrapper>
       <div className="chat-img">
-        <img src={gravatar.url(user.email)} alt={user.nickname} />
+        <img src={gravatar.url(user.email, { s: '36px', d: 'retro' })} alt={user.nickname} />
       </div>
       <div className="chat-text">
         <div className="chat-user">
           <b>{user.nickname}</b>
           <span>{dayjs(date).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
 };
 
-export default Chat;
+export default memo(Chat);
 
 export const ChatWrapper = styled.div`
   display: flex;
